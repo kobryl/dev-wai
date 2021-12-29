377ef11e500217b573572b66d7ccc1d11404dee0
kobryl <59873325+kobryl@users.noreply.github.com>
2021-12-29 13:47:39 +0100

    form

--- src/web/index.php
+++ src/web/index.php
@@ -10,11 +10,25 @@
     <main>
 
         <aside>
-            <form name="login" action="login.php" method="post">
-                <label for="username">Nazwa użytkownika</label>
-                <input type="text" name="username">
-                <label for="password">Hasło</label>
-                <input type="password" name="password">
+            <form name="login" method="post" action="login.php">
+                <fieldset>
+                    <label for="username">Nazwa użytkownika</label>
+                    <input type="text" id="username" name="username">
+                    <label for="password">Hasło</label>
+                    <input type="password" id="password" name="password">
+                    <input type="submit" value="Zaloguj">
+                </fieldset>
+            </form>
+            <form name="register" method="post" action="register.php">
+                <fieldset>
+                    <label for="new_user">Nazwa użytkownika</label>
+                    <input type="text" name="username" id="new_user">
+                    <label for="new_password">Hasło</label>
+                    <input type="password" id="new_password" name="password">
+                    <label for="new_rep_password">Hasło</label>
+                    <input type="password" id="new_rep_password" name="rep_password">
+                    <input type="submit" value="Zarejestruj">
+                </fieldset>
             </form>
         </aside>
     </main>

c41633a2c1f50a97fb03926cb68c93cbc2bbb9dc
kobryl <59873325+kobryl@users.noreply.github.com>
2021-12-29 13:09:45 +0100

    start

---
+++ src/web/index.php
@@ -0,0 +1,25 @@
+<html lang="pl">
+    <head>
+        <meta name="author" content="Konrad Bryłowski">
+        <title>Kawoświat</title>
+    </head>
+    <body>
+    <header>
+        <h1>Kawoświat</h1>
+    </header>
+    <main>
+
+        <aside>
+            <form name="login" action="login.php" method="post">
+                <label for="username">Nazwa użytkownika</label>
+                <input type="text" name="username">
+                <label for="password">Hasło</label>
+                <input type="password" name="password">
+            </form>
+        </aside>
+    </main>
+    <footer>
+
+    </footer>
+    </body>
+</html>
